52dc038913643bac1901fcabd998c192146a218c
Mohan Munisifreddy <mohanmunisifreddy@bitbucket.org>
2018-03-03 15:56:19 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var util = require('util');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,26 @@ app.get('/todos', (req, res) => {
 		res.send({todos});
 	}, (err) => {
 		res.status(400).send(err);
+	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	// res.send(req.params);
+	var id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		res.status(404).send();
+		return console.log('Invalid ID');
+	}
+
+	Todo.findById(id).then( (todo) => {
+		if (!todo) {
+			return res.status(404).send('ID not found');
+		}
+		res.send({todo});
+	}, (err) => {
+		// console.log(err);
+		res.status(400).send();
 	});
 });
 
