9ec89b3fb7e1db216ba103ad15089d3ba605b11d
Mohan Munisifreddy <mohanmunisifreddy@bitbucket.org>
2018-03-08 00:42:33 +0530

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -56,6 +56,26 @@ UserSchema.methods.generateAuthToken = function () {
 	});
 };
 
+UserSchema.statics.findByToken = function (token) {
+	var User = this;
+	var decoded;
+
+	try {
+		decoded = jwt.verify(token, 'abc123');
+	} catch(err) {
+		return Promise.reject();
+		// new Promise( (resolve, reject) => {
+		// 	reject();				
+		// });
+	}
+
+	return User.findOne({
+		'_id': decoded._id,
+		'tokens.token': token,
+		'tokens.access': 'auth'
+	});
+}
+
 var User = mongoose.model('users', UserSchema);
 
 module.exports = {User};
