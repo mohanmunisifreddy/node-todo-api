e85008cbd18f6959e600651369dd59c12bfe5138
Mohan Munisifreddy <mohanmunisifreddy@bitbucket.org>
2018-03-08 20:56:30 +0530

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,29 +1,34 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-	id: 10
-};
+var password = '123abc';
 
-var token = jwt.sign(data, '123mohan');
-console.log("Token:" + token);
+// bcrypt.genSalt(10, (err, salt) => {
+// 	console.log(salt);
+// 	bcrypt.hash(password, salt, (err, hash) => {
+// 		console.log(hash);
+// 	});
+// });
 
-var decoded = jwt.verify(token, '123mohan');
-console.log("Decoded", decoded);
+var hashedPassword = '$2a$10$qwVYRkvtSEJJ/Lah6KUfj.vzQq7ahfqAsxX8z3o0Z50XndoTSsiD.';
 
-
-
-
-
-
-
-
+bcrypt.compare(password, hashedPassword, (err, res) => {
+	console.log(res);
+});
 
 
 
 
+// var data = {
+// 	id: 10
+// };
 
+// var token = jwt.sign(data, '123mohan');
+// console.log("Token:" + token);
 
+// var decoded = jwt.verify(token, '123mohan');
+// console.log("Decoded", decoded);
 
 
 // var message = 'My name is Mohan';
