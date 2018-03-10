2604711d320f5c00039ee876c22e480d3ef4bec4
Mohan Munisifreddy <mohanmunisifreddy@bitbucket.org>
2018-03-10 12:35:52 +0530

    Added tests for DELETE /users/me/token

--- server/server.js
+++ server/server.js
@@ -129,7 +129,7 @@ app.get('/users/me', authenticate, (req, res) => {
 });
 
 app.delete('/users/me/token', authenticate, (req, res) => {
-	console.log(req);
+	// console.log(req);
 	req.user.removeToken(req.token).then( () => {
 		res.status(200).send();
 	}, () => {
